774045d71bed154022f3b527348e4f47827153e1
sminez <innes.andersonmorrison@gmail.com>
2022-11-22 16:29:22 +0000

    Deploying to gh-pages from @ sminez/actiserve@14baa3fe30feba678496f7b102d5cbbfb71da0fe 🚀

---
+++ source-files.js
@@ -0,0 +1,4 @@
+var sourcesIndex = JSON.parse('{\
+"actiserve":["",[["routes",[],["extractors.rs","inbox.rs","mod.rs","nodeinfo.rs","well_known.rs"]]],["client.rs","error.rs","main.rs","signature.rs","state.rs","util.rs"]]\
+}');
+createSourceSidebar();
